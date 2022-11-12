85bc21fc3fa72f6e06ba0a6063e4deadc25a7a3e
Aravind <aravindramsharma@gmail.com>
2022-11-12 15:25:32 +0530

    The final code is written along with the Start Game function which logs the score

--- RPS.js
+++ RPS.js
@@ -1,14 +1,18 @@
+let result = "";
+let playerScore = 0;
+let computerScore = 0;
+startGame();
 function getComputerChoice() {
   let choice = ["Rock", "Paper", "Scissor"];
-  let randomindex = Math.floor(Math.random() * choice.length);
-  let computerChoice = choice[randomindex].toLowerCase();
+  let randomIndex = Math.floor(Math.random() * choice.length);
+  let computerChoice = choice[randomIndex].toLowerCase();
   return computerChoice;
 }
-let result = "";
 // console.log(playerSelection);
 // console.log(computerSelection);
 // console.log(result);
 function playRound(playerSelection, computerSelection) {
+  // takes in two paramters: player and computer selection
   if (playerSelection == computerSelection) {
     result = "It's a tie, try Once more";
   } else if (playerSelection == "rock") {
@@ -23,23 +27,53 @@ function playRound(playerSelection, computerSelection) {
     } else {
       result = "You Win! Scissors cut paper";
     }
-  } else {
+  } else if (playerSelection == "paper") {
     if (computerSelection == "rock") {
       result = "You Win! Paper covers Rock";
     } else {
       result = "You lose! Scissors cut paper";
     }
+  } else {
+    result = "Enter a valid selection";
   }
   return result;
 }
-const playerSelection = prompt("Enter your Choice: ").toLowerCase();
-const computerSelection = getComputerChoice();
-playRound(playerSelection, computerSelection);
-console.log(result);
 
+// playRound(playerSelection, computerSelection);
+//console.log(result);
+
+function startGame() {
+  for (let i = 0; i < 5; i++) {
+    const playerSelection = prompt("Enter your Choice: ").toLowerCase();
+    const computerSelection = getComputerChoice();
+    playRound(playerSelection, computerSelection);
+    console.log(result);
+    if (result.includes("You Win")) {
+      playerScore++;
+    } else if (result.includes("You lose")) {
+      computerScore++;
+      // continue;
+    }
+  }
+  return playerScore, computerScore;
+}
+
+console.log(`Your score =  ${playerScore}`);
+console.log(`Computer score =  ${computerScore}`);
+if (playerScore == computerScore) {
+  console.log("Oh the Game is Tied!! Do you wanna play again ?");
+} else if (playerScore >= computerScore) {
+  console.log("You are the winner");
+} else {
+  console.log("Computer is the winner");
+}
 // Pseudo-code:
 // If playerselection == computerselection,then display "It's a tie, try Once more"
 // Else
 //  if playerselec = rock, then
 //  if computerselec = scissor, display "{playerselec} beats {computerselec}, you Won!"
 //  else, display "Shucks!, {computerselec} covers {playerselec} you loose!"
+
+// if result.cotains("you win")
+// player score ++
+// else computerscore ++
